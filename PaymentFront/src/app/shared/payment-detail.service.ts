93f912f003ae69104129351ad364d8279280f265
YiiSauce <youssefyouss321@gmail.com>
2024-01-25 23:42:36 +0100

    better

--- PaymentFront/src/app/shared/payment-detail.service.ts
+++ PaymentFront/src/app/shared/payment-detail.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentDetailService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  url:string = environment.apiBaseUrl+"/PaymentDetails";
+
+  refrechList(){
+    this.http.get(this.url).subscribe({
+      next:res=>{
+        console.log(res);
+      },
+      error:err=>{
+        console.log(err);
+      }
+    })
+  }
 }
