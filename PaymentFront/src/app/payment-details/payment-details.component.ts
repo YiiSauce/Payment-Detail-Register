4394a9bbc530accb5f995e6b30b6224bdca28b3d
YiiSauce <youssefyouss321@gmail.com>
2024-01-26 00:22:31 +0100

    better

--- PaymentFront/src/app/payment-details/payment-details.component.ts
+++ PaymentFront/src/app/payment-details/payment-details.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaymentDetailService } from '../shared/payment-detail.service';
 
 @Component({
   selector: 'app-payment-details',
   templateUrl: './payment-details.component.html',
-  styleUrls: ['./payment-details.component.css']
 })
-export class PaymentDetailsComponent {
+export class PaymentDetailsComponent implements OnInit{
+  constructor(private service:PaymentDetailService) {
+  }
+  ngOnInit(): void {
+    this.service.refrechList();
+  }
 
 }
--- PaymentFront/src/app/shared/payment-detail.service.ts
+++ PaymentFront/src/app/shared/payment-detail.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import{HttpClient} from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
+import { PaymentDetail } from './payment-detail.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,12 @@ export class PaymentDetailService {
 
   constructor(private http:HttpClient) { }
   url:string = environment.apiBaseUrl+"/PaymentDetails";
-
+  list:PaymentDetail[] = [];
   refrechList(){
     this.http.get(this.url).subscribe({
       next:res=>{
-        console.log(res);
+        this.list = res as PaymentDetail[];
+        console.log(this.list);
       },
       error:err=>{
         console.log(err);
