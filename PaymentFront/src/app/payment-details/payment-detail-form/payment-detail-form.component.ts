b77e91a2914e6812db9d65bd00062ad2782df364
YiiSauce <youssefyouss321@gmail.com>
2024-01-26 16:59:39 +0100

    Last changes

--- PaymentFront/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ PaymentFront/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -10,17 +10,36 @@ import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
   styleUrls: ['./payment-detail-form.component.css']
 })
 export class PaymentDetailFormComponent {
-  constructor(public service:PaymentDetailService, public toastr:ToastrService) {
+  constructor(public service: PaymentDetailService, public toastr: ToastrService) {
   }
 
-  onSubmit(form:NgForm){
+  onSubmit(form: NgForm) {
+    this.service.formSubmitted = true;
+    if (form.valid) {
+      if(this.service.formData.paymentDetailId == 0)
+        this.insertItem(form);
+      else
+        this.updateItem(form);
+    }
+  }
+  insertItem(form: NgForm){
     this.service.saveDetail().subscribe({
-      next:res=>{
+      next: res => {
         this.service.list = res as PaymentDetail[];
         this.service.resetForm(form);
-        this.toastr.success("Inserted Successfuly !","Payment Detail Register");
+        this.toastr.success("Inserted Successfuly !", "Payment Detail Register");
       },
-      error:err=>{console.log(err)}
+      error: err => { console.log(err) }
+    })
+  }
+  updateItem(form: NgForm){
+    this.service.updateDetail().subscribe({
+      next: res => {
+        this.service.list = res as PaymentDetail[];
+        this.service.resetForm(form);
+        this.toastr.info("Updated Successfuly !", "Payment Detail Register");
+      },
+      error: err => { console.log(err) }
     })
   }
 }
--- PaymentFront/src/app/shared/payment-detail.service.ts
+++ PaymentFront/src/app/shared/payment-detail.service.ts
@@ -8,11 +8,11 @@ import { NgForm } from '@angular/forms';
   providedIn: 'root'
 })
 export class PaymentDetailService {
-
   constructor(private http:HttpClient) { }
   url:string = environment.apiBaseUrl+"/PaymentDetails";
   list:PaymentDetail[] = [];
   formData :PaymentDetail = new PaymentDetail();
+  formSubmitted:boolean = false;
 
   refrechList(){
     this.http.get(this.url).subscribe({
@@ -27,8 +27,16 @@ export class PaymentDetailService {
   saveDetail(){
     return this.http.post(this.url, this.formData);
   }
+  
+  updateDetail(){
+    return this.http.put(this.url+`/${this.formData.paymentDetailId}`, this.formData);
+  }
+  deleteDetail(id:number){
+    return this.http.delete(this.url+'/'+id);
+  }
   resetForm(form:NgForm){
     form.form.reset();
     this.formData = new PaymentDetail();
+    this.formSubmitted = false;
   }
 }
