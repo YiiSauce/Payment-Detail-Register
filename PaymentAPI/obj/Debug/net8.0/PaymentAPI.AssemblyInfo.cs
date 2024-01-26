ca3ebe369c764b390d9b80beb20800e22e40fc0f
YiiSauce <youssefyouss321@gmail.com>
2024-01-26 02:10:31 +0100

    sec

--- PaymentAPI/obj/Debug/net8.0/PaymentAPI.AssemblyInfo.cs
+++ PaymentAPI/obj/Debug/net8.0/PaymentAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c41e1af2d550c81cc9abf96bb3bbca75e3005db3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93f912f003ae69104129351ad364d8279280f265")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
